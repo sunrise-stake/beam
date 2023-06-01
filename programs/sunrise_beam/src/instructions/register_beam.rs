53a5f88c6022fc225539835b99b9037bda3b3eb3
crustydev <ademola1145@gmail.com>
2023-06-01 17:06:00 +0100

    minor fixes

--- programs/sunrise_beam/src/instructions/register_beam.rs
+++ programs/sunrise_beam/src/instructions/register_beam.rs
@@ -1,9 +1,10 @@
 use crate::{state::BeamDetails, RegisterBeam};
 use anchor_lang::prelude::*;
 
-pub fn handler(ctx: Context<RegisterBeam>, beam_key: Pubkey) -> Result<()> {
+pub fn handler(ctx: Context<RegisterBeam>) -> Result<()> {
     let state = &mut ctx.accounts.state;
-    
+    let beam_key = ctx.accounts.beam_account.key();
+
     state.add_beam(BeamDetails::new(beam_key, 0))?;
 
     Ok(())
