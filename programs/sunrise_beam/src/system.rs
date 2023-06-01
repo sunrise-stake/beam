709eb95f67e689b93640b7387b2671430c3e9a97
crustydev <ademola1145@gmail.com>
2023-06-01 17:04:31 +0100

    feat: Add separate handler for resizing state account

--- programs/sunrise_beam/src/system.rs
+++ programs/sunrise_beam/src/system.rs
@@ -1,9 +1,5 @@
-use crate::{
-    state::{BeamDetails, State},
-    utils::resize_account,
-    BeamError,
-};
 use anchor_lang::prelude::*;
+use crate::{state::State, BeamError};
 
 /// Verifies that a mint request is valid by:
 /// - Checking that the beam is present in the state.
@@ -19,32 +15,6 @@ pub fn check_beam_validity(
     if beam.owner != cpi_program_id {
         return Err(BeamError::UnidentifiedCallingProgram.into());
     }
-
-    Ok(())
-}
-
-#[allow(dead_code)]
-pub fn resize_state<'a>(
-    state: &mut Account<'a, State>,
-    payer: &AccountInfo<'a>,
-    system_program: &AccountInfo<'a>,
-) -> Result<()> {
-    let window = state.alloc_window;
-    if window == 0 {
-        return Err(BeamError::WouldExceedBeamCapacity.into());
-    }
-
-    let new_length = state
-        .allocations
-        .len()
-        .checked_add(window as usize)
-        .unwrap();
-    let new_size = State::size(new_length);
-
-    resize_account(&state.to_account_info(), payer, system_program, new_size)?;
-    state
-        .allocations
-        .extend(std::iter::repeat(BeamDetails::default()));
 
     Ok(())
 }
