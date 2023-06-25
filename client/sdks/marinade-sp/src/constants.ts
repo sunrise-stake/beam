39ab2514533ebe85300936a2ca1673e793726d53
Ademola <ademolafatoke@gmail.com>
2023-06-25 23:58:00 +0100

    feat: Add basic client support for marinade beam

---
+++ client/sdks/marinade-sp/src/constants.ts
@@ -0,0 +1,11 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const MARINADE_BEAM_PROGRAM_ID = new PublicKey(
+  "G9nMA5HvMa1HLXy1DBA3biH445Zxb2dkqsG4eDfcvgjm"
+);
+export const MARINADE_FINANCE_PROGRAM_ID = new PublicKey(
+  "MarBmsSgKXdrN1egZf5sqe1TMai9K1rChYNDJgjq7aD"
+);
+
+export const STATE_SEED = "sunrise-marinade";
+export const VAULT_AUTHORITY_SEED = "vault-authority";
