e54074be0f83dd95c4e7c2ea2c396b4df778b805
Ademola <ademolafatoke@gmail.com>
2023-06-30 10:46:30 +0100

    feat: spl beam sdk

---
+++ packages/sdks/spl/src/state.ts
@@ -0,0 +1,47 @@
+import { type PublicKey } from "@solana/web3.js";
+import { type IdlAccounts, type BN } from "@coral-xyz/anchor";
+import { type SplBeam } from "../../types/spl_beam";
+import { type BeamState } from "../../sunrise/src/beamInterface";
+
+export class StateAccount implements BeamState {
+  public readonly address: PublicKey;
+  public readonly updateAuthority: PublicKey;
+  public readonly proxyState: PublicKey;
+  public readonly sunriseState: PublicKey;
+  public readonly vaultAuthorityBump: number;
+  public readonly treasury: PublicKey;
+
+  private constructor(
+    _address: PublicKey,
+    account: IdlAccounts<SplBeam>["state"]
+  ) {
+    this.address = _address;
+    this.updateAuthority = account.updateAuthority;
+    this.proxyState = account.stakePool;
+    this.sunriseState = account.sunriseState;
+    this.vaultAuthorityBump = account.vaultAuthorityBump;
+    this.treasury = account.treasury;
+  }
+
+  /** Create a new instance from an anchor-deserialized account. */
+  public static fromIdlAccount(
+    account: IdlAccounts<SplBeam>["state"],
+    address: PublicKey
+  ): StateAccount {
+    return new StateAccount(address, account);
+  }
+
+  /** Pretty print. */
+  public pretty(): {
+    [Property in keyof Omit<StateAccount, "pretty">]: string;
+  } {
+    return {
+      address: this.address.toBase58(),
+      updateAuthority: this.updateAuthority.toBase58(),
+      proxyState: this.proxyState.toBase58(),
+      sunriseState: this.sunriseState.toBase58(),
+      vaultAuthorityBump: this.vaultAuthorityBump.toString(),
+      treasury: this.treasury.toBase58(),
+    };
+  }
+}
