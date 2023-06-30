e906e997426f30aa121b7efb271ca9666cbd963c
Ademola <ademolafatoke@gmail.com>
2023-06-30 10:39:15 +0100

    marinade-lp: Initialize token account in initialize instruction + box accounts + add StateEntry type

--- programs/marinade-lp-beam/src/state.rs
+++ programs/marinade-lp-beam/src/state.rs
@@ -31,3 +31,28 @@ impl State {
         32 + /*treasury*/
         32; /*msol-token-account*/
 }
+
+// Anchor-ts only deserializes for instruction arguments types that explicitly derive
+// AnchorSerialize & AnchorDeserialize.
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct StateEntry {
+    pub update_authority: Pubkey,
+    pub marinade_state: Pubkey,
+    pub sunrise_state: Pubkey,
+    pub vault_authority_bump: u8,
+    pub treasury: Pubkey,
+    pub msol_token_account: Pubkey,
+}
+
+impl From<StateEntry> for State {
+    fn from(se: StateEntry) -> Self {
+        State {
+            update_authority: se.update_authority,
+            marinade_state: se.marinade_state,
+            sunrise_state: se.sunrise_state,
+            vault_authority_bump: se.vault_authority_bump,
+            treasury: se.treasury,
+            msol_token_account: se.msol_token_account,
+        }
+    }
+}
